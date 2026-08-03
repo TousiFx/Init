22e4c137476260d7764ffa9c72b838f19a52f75d
TousifX <mr.trexyt9@gmail.com>
2026-08-03 12:02:38 +0530

    using log feature

--- git.js
+++ git.js
@@ -1,2 +1,8 @@
 // add new feature -button
 // add new feature -from
+
+// mistake
+console.log("Check this console.log");
+const para = document.getElementById("para");
+
+para.textContent = "Hello change the mina core of this line";
